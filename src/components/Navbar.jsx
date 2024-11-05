32e76a33f92d3e3a09534c5cc42f8acf1f4fbeb4
Diego Lázaro <dlazarodecastro@gmail.com>
2024-11-05 20:05:30 +0100

    first step - navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <p>About me</p>
+      <p>Projects</p>
+      <p>Skills</p>
+      <p>Contact</p>
+    </div>
+  )
+}
+
+export default Navbar
